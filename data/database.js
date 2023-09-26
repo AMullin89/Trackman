1019439ee7287ddee4fbef1a0f3eb03fab0e2b36
Ashley Mullin <aetmullin@hotmail.com>
2023-09-26 11:49:42 +0100

    connecting to jawsdb

--- data/database.js
+++ data/database.js
@@ -2,10 +2,10 @@ const mysql = require('mysql2/promise');
 
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD
+    host: 'oliadkuxrl9xdugh.chr7pe7iynqr.eu-west-1.rds.amazonaws.com',
+    database: 'eucrzbyy9inxtgbj',
+    user: 'b2j43yb83gtnq0af',
+    password: 'mqh22vk93bq39ey9'
 });
 
 module.exports = pool;
